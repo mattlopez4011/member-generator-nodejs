72e620eaecd4ec5ab8d9744a002c70abae93c5b4
Bonee94 <bonee94@yahoo.com>
2022-12-18 02:01:44 -0600

    Question prompts completed

--- index.js
+++ index.js
@@ -1,9 +1,106 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const TeamMember = require('./lib/classes');
 
+const membersArray = [];
+
+const internQuestions = [
+    {
+        type: 'input',
+        message: 'Please enter your name',
+        name: 'memberName',
+    },
+    {
+        type: 'input',
+        message: 'Please enter your email adress',
+        name: 'memberEmail',
+    },
+    {
+        type: 'input',
+        message: 'Please enter your school',
+        name: 'memberSchool',
+    },
+    {
+        type: 'list',
+        message: 'Would you like to add another team meaber?',
+        name: 'createMember',
+        choices: ['Add Engineer', 'Add Intern', 'Finish building team'],
+    },
+]
+
+const engineerQuestions = [
+    {
+        type: 'input',
+        message: 'Please enter your name',
+        name: 'memberName',
+    },
+    {
+        type: 'input',
+        message: 'Please enter your email adress',
+        name: 'memberEmail',
+    },
+    {
+        type: 'input',
+        message: 'Please enter your Github username',
+        name: 'memberGithubUsername',
+    },
+    {
+        type: 'list',
+        message: 'Would you like to add another team meaber?',
+        name: 'createMember',
+        choices: ['Add Engineer', 'Add Intern', 'Finish building team'],
+    },
+]
+
+const managerQuestions = [
+    {
+        type: 'input',
+        message: 'Please enter your name',
+        name: 'memberName',
+    },
+    {
+        type: 'input',
+        message: 'Please enter your email adress',
+        name: 'memberEmail',
+    },
+    {
+        type: 'input',
+        message: 'Please enter your office number',
+        name: 'managerOfficeNumber',
+    },
+    {
+        type: 'list',
+        message: 'Would you like to add another team meaber?',
+        name: 'createMember',
+        choices: ['Add Engineer', 'Add Intern', 'Finish building team'],
+    },
+]
 
 //team manager’s name, employee ID, email address, and office number
-inquirer
-.prompt(
 
-)
+
+const memberInfo = questions => {
+    inquirer
+        .prompt(
+            questions
+        )
+        .then(response => {
+            membersArray.push(response);
+            console.log('\n')
+
+            if (response.createMember === "Add Engineer") {
+                memberInfo(engineerQuestions);
+            } else if (response.createMember === "Add Intern") {
+                memberInfo(internQuestions);
+            } else if (response.createMember === "Finish building team") {
+                console.log(`Team is Finished!`)
+            }
+        })
+}
+
+
+const init = () => {
+    memberInfo(managerQuestions);
+}
+
+init();
